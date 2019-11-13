35b34e46404633c3d702a7f4f3ea2c1a056f176b
treepoint <owner-of-all-real@ya.ru>
2019-11-13 13:54:05 +0500

    form validation in form itself but in inputs

--- src/Components/Registration.js
+++ src/Components/Registration.js
@@ -16,42 +16,75 @@ class Registration extends React.Component {
     this.setState({ [event.target.name]: event.target.value });
   }
 
-  onSubmit(event) {
-    this.setState({ isTouched: true });
+  //Собираем значения в объект
+  getValuesAsObject() {
+    let values = {};
+    REG_INPUTS.forEach(input => {
+      values[input.name] = this.state[input.name];
+    });
+    return values;
+  }
 
-    //Выводим все существующее в консоль списком
-    REG_INPUTS.forEach(item => {
-      if (!!this.state[item.name]) {
-        console.log(item.name + ":" + this.state[item.name]);
-      }
+  //Собираем значения валидации в ассоциативный массив
+  getInvalidMessagesAsAssocArray() {
+    let validation = new Map();
+
+    REG_INPUTS.forEach(input => {
+      validation.set(
+        input.name,
+        input.validationFunctions
+          .map(func => func.getInvalidMessage(this.state[input.name]))
+          .filter(value => value)
+      );
+
+      return validation;
     });
 
+    return validation;
+  }
+
+  //Получим количество ошибок в формеы
+  getFormErrorCount() {
+    let cnt = 0;
+    this.state.validation.forEach(item => {
+      cnt = cnt + Object.keys(item).length;
+    });
+    return cnt;
+  }
+
+  onSubmit(event) {
     //Отключаем стандартный обработчик данного события
     event.preventDefault();
+
+    this.setState(
+      { isTouched: true, validation: this.getInvalidMessagesAsAssocArray() },
+      () => {
+        //Шлем в консоль только если все корректно
+        if (this.getFormErrorCount() === 0) {
+          console.log(this.getValuesAsObject());
+        }
+      }
+    );
   }
 
   render() {
-    var regInputs = REG_INPUTS.map(regInputs => (
-      <Input
-        placeholder={regInputs.placeholder}
-        name={regInputs.name}
-        value={this.state[regInputs.name]}
-        defaultValue={regInputs.defaultValue}
-        onChange={event => this.onChange(event)}
-        validationFunctions={regInputs.validationFunctions}
-        isTouched={this.state.isTouched}
-      />
-    ));
-
-    //Если кнопкой было установлено состоение в true, то сразу после рендеринга сменим его на false.
-    if (this.state.isTouched) {
-      this.setState({ isTouched: false });
-    }
-
     return (
       <form onSubmit={this.onSubmit} className="Registration">
         <h1 className="h1">Расскажи о себе</h1>
-        {regInputs}
+        {REG_INPUTS.map(regInputs => (
+          <Input
+            placeholder={regInputs.placeholder}
+            name={regInputs.name}
+            value={this.state[regInputs.name]}
+            defaultValue={regInputs.defaultValue}
+            onChange={event => this.onChange(event)}
+            invalidMessage={
+              !!this.state.isTouched
+                ? this.state.validation.get(regInputs.name)[0]
+                : ""
+            }
+          />
+        ))}
         <Button value="ОТПРАВИТЬ" />
       </form>
     );
--- src/Elements/Input/Input.js
+++ src/Elements/Input/Input.js
@@ -3,31 +3,12 @@ import LableValidation from "../LableValidation/LableValidation";
 import "./Input.css";
 
 class Input extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { invalidMessage: "" };
-  }
   render() {
-    //Проходимся по массиву функций валидации. Если что-то вернулось — значит поле не валидное
-    let isNotValid = this.props.validationFunctions.find(
-      item =>
-        item.getInvalidMessage(this.props.value) !== "" && this.props.isTouched
-    );
-
-    //Если нажали на кнопку — обновляем сообщение об ошибке. В противном случае только рендерим новое value
-    if (this.props.isTouched) {
-      this.setState({
-        invalidMessage: !!isNotValid
-          ? isNotValid.getInvalidMessage(this.props.value)
-          : ""
-      });
-    }
-
     return (
       <div>
         <label style={{ display: "flex" }}>
           <input
-            className={"input" + (this.state.invalidMessage ? " error" : "")}
+            className={"input" + (this.props.invalidMessage ? " error" : "")}
             type="text"
             name={this.props.name}
             defaultValue={this.props.defaultValue}
@@ -37,7 +18,7 @@ class Input extends React.Component {
             }}
           />
         </label>
-        <LableValidation value={this.state.invalidMessage} />
+        <LableValidation value={this.props.invalidMessage} />
       </div>
     );
   }
