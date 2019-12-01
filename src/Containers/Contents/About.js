5b1e71d5d917d1daf064911093376bd164d2e7f8
Paul Khoziashev <owner-of-all-real@ya.ru>
2019-12-01 14:14:18 +0500

    add information to about page and make dynamic broadcast message height

--- src/Containers/Contents/About.js
+++ src/Containers/Contents/About.js
@@ -6,11 +6,28 @@ class About extends React.Component {
     return (
       <Page title="Справка">
         <p>
-          Этот проект написан на React и есть на Github →{" "}
-          <a href="https://github.com/treepoint/reactFront">
-            https://github.com/treepoint/reactFront
-          </a>
-          .
+          Этот проект написан на React, NodeJS и использует MySQL как базу
+          данных. Все есть на Github:
+          <ul>
+            <li>
+              Фронт —{" "}
+              <a href="https://github.com/treepoint/haveDone">
+                https://github.com/treepoint/haveDone
+              </a>
+            </li>
+            <li>
+              API —{" "}
+              <a href="https://github.com/treepoint/haveDoneAPI">
+                https://github.com/treepoint/haveDoneAPI
+              </a>
+            </li>
+            <li>
+              Скрипты MySQL —{" "}
+              <a href="https://github.com/treepoint/haveDoneMySQL">
+                https://github.com/treepoint/haveDoneMySQL
+              </a>
+            </li>
+          </ul>
         </p>
         <p>
           После каждого коммита происходит сборка через jenkins. Последняя
