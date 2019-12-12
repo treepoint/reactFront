2468eba9a8e0a98cde2fcaf25c4cf46bab2855fd
Paul Khoziashev <owner-of-all-real@ya.ru>
2019-12-12 21:14:54 +0500

    uuid for Cell + chosen and unchosen functionality

--- src/Components/Table/Cell/Cell.js
+++ src/Components/Table/Cell/Cell.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ContentEditable from "react-contenteditable";
+import uuid from "uuid/v4";
 import { Resizable } from "re-resizable";
 import "./Cell.css";
 
@@ -7,21 +8,40 @@ class Cell extends React.Component {
   constructor() {
     super();
     this.state = {
-      htmlContent: " "
+      htmlContent: " ",
+      className: "inner",
+      uuid: ""
     };
   }
 
-  //Добавляем в ячейку контент, если он есть
   componentDidMount() {
+    //Добавляем в ячейку контент, если он есть
     if (typeof this.props.htmlContent !== "undefined") {
       //Чтобы нам не прилетело — мы здесь со строками работаем
       this.setState({ htmlContent: String(this.props.htmlContent) });
     }
+
+    //Генерируем UUID для ячейки
+    this.setState({ uuid: uuid() });
   }
 
   //Изменяем контент по вводу
   onChange(event) {
     this.setState({ htmlContent: event.target.value });
+  }
+
+  //Срабатывает при двойном клике
+  setChosenClassName() {
+    if (!this.props.isEditable) {
+      return;
+    }
+
+    this.setState({ className: "inner chosen" });
+  }
+
+  //Срабатывает при потере фокуса
+  setDefaultClassName() {
+    this.setState({ className: "inner" });
   }
 
   render() {
@@ -63,11 +83,14 @@ class Cell extends React.Component {
     return (
       <Resizable
         className={!!this.props.isTh ? "th" : "td"}
+        uuid={this.state.uuid}
         //Выставляем размеры ячейки
         size={{
           width: this.state.width + "px",
           height: this.state.height + "px"
         }}
+        //Задаем минимальную высоту
+        minHeight={34}
         //Указываем какие грани ячейки активны для изменения размеров
         enable={enable}
         //При резайзе отправляем размеры вверх, в строку
@@ -83,7 +106,7 @@ class Cell extends React.Component {
         {/*Отображаем само поле с редактируемым контентом */}
         <ContentEditable
           spellcheck="false"
-          className="inner"
+          className={this.state.className}
           style={{
             //Подгоняем размеры внутреннего контента по размеры ячейки, но компенсируем отступы и бордюры
             width: this.state.width - 5 + "px",
@@ -94,6 +117,10 @@ class Cell extends React.Component {
           //Задаем редактируемость. Не редактируем === отключено
           disabled={!this.props.isEditable}
           onChange={event => this.onChange(event)}
+          //Обрабатываем двойной клик
+          onDoubleClick={event => this.setChosenClassName(event)}
+          //При уходе фокуса задаем стандартный стиль. Нужно чтобы сбросить последствия двойного клика
+          onBlur={event => this.setDefaultClassName(event)}
         ></ContentEditable>
       </Resizable>
     );
