d90129f1fda60042bc230e69185bfb62ff5f272c
Paul Khoziashev <owner-of-all-real@ya.ru>
2020-01-05 17:04:15 +0500

    outer content update for text content

--- src/Components/Table/Cell/TextContent/TextContent.js
+++ src/Components/Table/Cell/TextContent/TextContent.js
@@ -18,6 +18,12 @@ class TextContent extends React.Component {
     };
   }
 
+  componentDidUpdate() {
+    if (this.state.content !== this.props.content && this.state.isReadOnly) {
+      this.setState({ content: this.props.content });
+    }
+  }
+
   componentDidMount() {
     this.setState({ content: this.props.content });
   }
