0f61c6ecb0f274576a912bef199f2a2a166ff955
Paul Khoziashev <owner-of-all-real@ya.ru>
2020-01-27 21:45:29 +0500

    change version

--- src/Containers/Bottom/Bottom.js
+++ src/Containers/Bottom/Bottom.js
@@ -5,7 +5,7 @@ class Bottom extends React.PureComponent {
   render() {
     return (
       <div className="bottom">
-        <BroadCastMessage message="Веб-приложение находится в разработке. Рекомендуемый браузер — Firefox. Номер сборки: 0.6.2" />
+        <BroadCastMessage message="Веб-приложение находится в разработке. Рекомендуемый браузер — Firefox. Номер сборки: 0.6.3" />
       </div>
     );
   }
