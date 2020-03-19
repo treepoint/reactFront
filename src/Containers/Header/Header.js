c17f8fbd1e3abded9443efa3eafdfbb030993ff7
treepoint <owner-of-all-real@ya.ru>
2020-03-20 01:24:22 +0500

    change version

--- src/Containers/Header/Header.js
+++ src/Containers/Header/Header.js
@@ -34,7 +34,7 @@ class Header extends React.PureComponent {
                 Веб-приложение находится в разработке. Рекомендуемый браузер —
                 Firefox.
               </p>
-              <p>Номер сборки: 0.8.0</p>
+              <p>Номер сборки: 0.8.1</p>
             </React.Fragment>
           }
         />
