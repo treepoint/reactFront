4ec4f9f333b8f33f9c8f7ead8fc98e0c4c2b6b71
Paul Khoziashev <hozyashev@tados.ru>
2021-05-21 12:52:28 +0500

    remove useless things

--- src/Components/AddTaskButton/Steps/StartStep.js
+++ src/Components/AddTaskButton/Steps/StartStep.js
@@ -1,7 +1,6 @@
 import React from "react";
 //Картинки
 import addIcon from "../../../Images/icon_add_96.png";
-import addWTimeIcon from "../../../Images/icon_add_w_time_96.png";
 //CSS
 import "./StartStep.css";
 
